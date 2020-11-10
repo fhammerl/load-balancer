b194687aa1626abcded06658748806b1eb997be0
Ferenc Hammerl <hammerl.ferenc@gmail.com>
2020-11-10 19:52:27 +0000

    Add Provider class (Step 1)

---
+++ LoadBalancer/Provider.cs
@@ -0,0 +1,21 @@
+﻿namespace LoadBalancer
+{
+    public interface IProvider
+    {
+        string Get();
+    }
+
+    public class Provider : IProvider
+    {
+        private string id;
+
+        public Provider(string id)
+        {
+            this.id = id;
+        }
+        public string Get()
+        {
+            return id;
+        }
+    }
+}
