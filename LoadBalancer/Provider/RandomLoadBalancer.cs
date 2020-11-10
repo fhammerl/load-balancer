a624d8a1d7321dc20ea896e6caa9e7068a6b08eb
Ferenc Hammerl <hammerl.ferenc@gmail.com>
2020-11-10 20:42:06 +0000

    Add RandomLoadBalancer.cs

---
+++ LoadBalancer/Provider/RandomLoadBalancer.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LoadBalancer
+{
+    public class RandomLoadBalancer : LoadBalancer
+    {
+        public override string Get()
+        {
+            return this.Providers[new Random().Next(0, Providers.Count)].Get();
+        }
+    }
+}
