4c2512a1cd6390aa9a69c6d43810af7900e2239d
Ferenc Hammerl <hammerl.ferenc@gmail.com>
2020-11-10 20:37:58 +0000

    Add Provider registration to LoadBalancer (Step 2)
    I made the ILoadBalancer interface extend the IProvider interface as it allows  can imagine a scenario of LoadBalancers pointing to other LoadBalancers.

--- LoadBalancer/LoadBalancer.cs
+++ LoadBalancer/LoadBalancer.cs
@@ -1,8 +1,32 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace LoadBalancer
 {
-    public class LoadBalancer
+    public interface ILoadBalancer : IProvider
     {
+        void Register(IList<Provider> providers);
+    }
+
+    public abstract class LoadBalancer : ILoadBalancer
+    {
+        protected IList<Provider> Providers;
+        const int MaxLength = 10;
+
+        public void Register(IList<Provider> providers)
+        {
+            if (providers.Count > MaxLength)
+            {
+                throw new ArgumentException($"Can't have more than {MaxLength} providers");
+            }
+            if (!providers.Any())
+            {
+                throw new ArgumentException($"Can't have 0 providers");
+            }
+            Providers = providers;
+        }
+
+        public abstract string Get();
     }
 }
