acc8aba1d31f517a512d4dce882737778f091c8d
Ferenc Hammerl <hammerl.ferenc@gmail.com>
2020-11-18 21:16:40 +0000

    Add randomGenerator to randomStrategy

--- LoadBalancer/Strategies/Random/RandomStrategy.cs
+++ LoadBalancer/Strategies/Random/RandomStrategy.cs
@@ -1,12 +1,17 @@
 ﻿using LoadBalancer.Providers;
 using System;
 using System.Collections.Generic;
-using System.Text;
 
 namespace LoadBalancer.LoadBalancer.Strategies
 {
     public class RandomLoadBalancerStrategy : ILoadBalancerStrategy
     {
+        private readonly IRandomNumberGenerator random;
+
+        public RandomLoadBalancerStrategy(IRandomNumberGenerator random)
+        {
+            this.random = random;
+        }
         public IProvider SelectProvider(IList<IProvider> providers)
         {
             throw new NotImplementedException();
